900cc9522e70487758060f812b79b0c882067f1d
ilinaodouard <icodouard@gmail.com>
2024-11-05 19:21:42 -0500

    associated files

---
+++ website/.vdoc.r
@@ -0,0 +1,44 @@
+#
+#
+#
+#
+#
+#
+#
+# | include: false
+knitr::include_graphics("Odouard_Ilina.jpeg")
+#
+#
+#
+#
+#
+#
+#
+#
+#| echo: false
+knitr::include_graphics("riverside.jpeg")
+#
+#
+#
+#
+#| echo: false
+knitr::include_graphics("cp.jpeg")
+#
+#
+#
+#
+#| echo: false
+knitr::include_graphics("canton.jpeg")
+#
+#
+#
+#
+#| echo: false
+knitr::include_graphics("patterson.jpeg")
+#
+#
+#
+#
+#
+#
+#
